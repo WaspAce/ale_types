3087e170b3b249f11dc5863706122dbd281d81b3
Architector WASPACE <architector.waspace@gmail.com>
2018-10-03 12:24:18 +0300

    Typo fixed

--- index.ts
+++ index.ts
@@ -1,7 +1,5 @@
-import './ale_types/rect/align_type.t';
-import './ale_types/rect/anchor_kind.t';
-import './ale_types/rect/auth_callback.t';
-import './ale_types/rect/binary_value.t';
+import './ale_types/align_type/align_type.t';
+import './ale_types/anchor_kind/anchor_kind.t';
+import './ale_types/auth_callback/auth_callback.t';
 import './ale_types/point/point.t';
 import './ale_types/rect/rect.t';
-
