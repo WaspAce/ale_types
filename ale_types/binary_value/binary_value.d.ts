97d58cb048245c12fbb2d9b3efa44ca21145f7dc
Architector WASPACE <architector.waspace@gmail.com>
2018-10-03 12:45:13 +0300

    BinaryValue declared

--- ale_types/binary_value/binary_value.d.ts
+++ ale_types/binary_value/binary_value.d.ts
@@ -1,2 +1,14 @@
 declare class BinaryValue {
+  constructor(
+    data_bytes: number[]
+  );
+
+  readonly size: number;
+  readonly is_valid: boolean;
+  readonly is_owned: boolean;
+
+  is_same(that: BinaryValue): boolean;
+  is_equal(that: BinaryValue): boolean;
+  copy(): BinaryValue;
+  get_data(): number[];
 }
