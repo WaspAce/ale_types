2f94de84c7ec5cf6a4f7a8ab05c364a4ac9c2b42
Architector WASPACE <architector.waspace@gmail.com>
2018-10-04 14:16:42 +0300

    docs

--- ale_types/binary_value/binary_value.d.ts
+++ ale_types/binary_value/binary_value.d.ts
@@ -16,39 +16,39 @@ declare class BinaryValue {
   readonly size: number;
 
   /**
-   * true if this object is valid. This object may become invalid if
+   * **true** if this object is valid. This object may become invalid if
    * the underlying data is owned by another object (e.g. list or dictionary)
    * and that other object is then modified or destroyed. Do not call any other
-   * functions if this is false.
+   * functions if this is **false**.
    */
   readonly is_valid: boolean;
 
   /**
-   * true if this object is currently owned by another object.
+   * **true** if this object is currently owned by another object.
    */
   readonly is_owned: boolean;
 
   /**
-   * Returns true if this object and **that** object have the same underlying
+   * @returns **true** if this object and **that** object have the same underlying
    * data.
    */
   is_same(that: BinaryValue): boolean;
 
   /**
-   * Returns true if this object and **that** object have an equivalent
+   * @returns **true** if this object and **that** object have an equivalent
    * underlying value but are not necessarily the same object.
    */
   is_equal(that: BinaryValue): boolean;
 
   /**
-   * Returns a copy of this object. The data in this object will also be copied.
+   * @returns A copy of this object. The data in this object will also be copied.
    */
   copy(): BinaryValue;
 
   /**
    * Read up to **buffer_size** number of bytes. Reading begins at
    * the specified byte **data_offset**.
-   * @returns read bytes.
+   * @returns Read bytes.
    */
   get_data(buffer_size: number, data_offset: number): number[];
 }
