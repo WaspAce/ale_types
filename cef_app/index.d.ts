137c993287459379fe4bf0bb19661d262432ed3e
Architector WASPACE <architector.waspace@gmail.com>
2018-10-10 12:23:07 +0300

    CEF_APP declared

--- cef_app/index.d.ts
+++ cef_app/index.d.ts
@@ -1,7 +1,46 @@
-declare class CefApp {
+declare class CEF_APP {
+
+  /**
+   * 
+   */
+  readonly settings: CefAppSettings;
+
+  /**
+   * 
+   */
+  loop_interval_ms: number;
+
+  /**
+   * 
+   */
+  external_message_pump: boolean;
+
+  /**
+   * 
+   */
+  readonly initialized: boolean;
+
+
+  /**
+   * 
+   */
+  static init(
+    settings: CefAppSettings
+  );
+
+  /**
+   * 
+   */
+  get_global_request_context(): RequestContext;
+
+  /**
+   * 
+   */
+  get_global_cookie_manager(): CookieManager;
+
   /**
    * Asynchronously create a new browser window using the window parameters specified by
-   * |windowInfo|. All values will be copied internally.
+   * |window_info|. All values will be copied internally.
    * @param request_context If is null the global request context will be used.
    */
   static create_browser(
