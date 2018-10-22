44e14f10c7d887b64e69a74eb9d2dead6c341ced
Architector WASPACE <architector.waspace@gmail.com>
2018-10-22 14:43:25 +0300

    GuiForm events added

--- gui_form/index.d.ts
+++ gui_form/index.d.ts
@@ -11,4 +11,20 @@ declare class GuiForm extends GuiControl {
    * Close the window.
    */
   close(): void;
+
+  /**
+   * Called when window is closed.
+   * @event
+   */
+  on_close: () => void;
+
+  /**
+   * Called when window is about to close.
+   * @event
+   */
+  on_close_query:
+  /**
+   * @return Return true to allow window close.
+   */
+  () => boolean;
 }
