862322bf6d69ecb7f42cfccec8c6fec088687622
Architector WASPACE <architector.waspace@gmail.com>
2018-10-23 12:03:47 +0300

    subprocess start added

--- subprocess/index.d.ts
+++ subprocess/index.d.ts
@@ -6,4 +6,9 @@ declare class subprocess {
    * The handler for functionality specific to the render process.
    */
   static render_process_handler: RenderProcessHandler;
+
+  /**
+   * 
+   */
+  static start();
 }
