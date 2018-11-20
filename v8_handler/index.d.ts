8c7de79bfdc2d60fba8d3ba28777331677a77d95
Architector WASPACE <architector.waspace@gmail.com>
2018-11-20 11:31:22 +0300

    V8Handler execute cannot return any value

--- v8_handler/index.d.ts
+++ v8_handler/index.d.ts
@@ -19,20 +19,11 @@ declare class V8Handler {
    * @param context Execution context.
    * @param object The receiver ('this' object) of the function.
    * @param arguments The list of arguments passed to the function.
-   * @param ret_val If execution succeeds set |ret_val| to the
-	 * function return value.
-   * @param exception If execution fails set |exception| to the exception
-	 * that will be thrown.
-   * @param result Set to true if execution was handled.
    */
   (
     context: V8Context,
     name: string,
     object: V8Value,
     arguments: V8Value[]
-  ) => {
-    result: boolean,
-    ret_val: V8Value,
-    exception: string
-  }
+  ) => void;
 }
