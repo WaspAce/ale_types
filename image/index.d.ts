9e48ed09c17ada8cf4db93c99cfbb5a38e04f83a
Architector WASPACE <architector.waspace@gmail.com>
2019-11-20 12:51:36 +0300

    add some context functionality to Image

--- image/index.d.ts
+++ image/index.d.ts
@@ -64,5 +64,163 @@ declare class Image {
    * Clears image.
    */
   clear(): void;
+
+
+  shadowBlur: number;
+  shadowColor: string;
+  shadowOffsetX: number;
+  shadowOffsetY: number;
+  lineCap: string;
+  lineJoin: string;
+  lineWidth: number;
+  miterLimit: number;
+
+  setFillStyle(
+    value: string
+  );
+  setStrokeStyle(
+    value: string
+  );
+  createLinearGradient(
+    x0: number,
+    y0: number,
+    x1: number,
+    y1: number
+  );
+
+  createPattern(
+    image: Image,
+    repetition: 'repeat' | 'repeat-x' | 'repeat-y' | 'no-repeat'
+  );
+
+  createRadialGradient(
+    x0: number,
+    y0: number,
+    r0: number,
+    x1: number,
+    y1: number,
+    r1: number
+  );
+
+  clearRect(
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  );
+
+  fillRect(
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  );
+
+  rect(
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  );
+
+  strokeRect(
+    x: number,
+    y: number,
+    width: number,
+    height: number
+  );
+
+  arc(
+    x: number,
+    y: number,
+    r: number,
+    sAngle: number,
+    eAngle: number,
+    counterclockwise?: boolean
+  );
+
+  arcTo(
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    r: number
+  );
+
+  beginPath();
+
+  bezierCurveTo(
+    p1x: number,
+    cp1y: number,
+    cp2x: number,
+    cp2y: number,
+    x: number,
+    y: number
+  );
+
+  clip();
+
+  closePath();
+
+  fill();
+
+  isPointInPath(
+    x: number,
+    y: number
+  );
+
+  lineTo(
+    x: number,
+    y: number
+  );
+
+  moveTo(
+    x: number,
+    y: number
+  );
+
+  quadraticCurveTo(
+    cpx: number,
+    cpy: number,
+    x: number,
+    y: number
+  );
+
+  stroke();
+
+  rotate(
+    angle: number
+  );
+
+  scale(
+    scalewidth: number,
+    scaleheight: number
+  );
+
+  setTransform(
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    e: number,
+    f: number
+  );
+
+  transform(
+    a: number,
+    b: number,
+    c: number,
+    d: number,
+    e: number,
+    f: number
+  );
+
+  translate(
+    x: number,
+    y: number
+  );
+
+  toDataURL(): string;
+
 }
 
